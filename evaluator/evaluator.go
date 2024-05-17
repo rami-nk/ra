7196561c9ca083ed1b5a7a0a1fd7b2219f738908
Rami Nakkar <rami.nakkar@check24.de>
2024-05-17 22:37:43 +0200

    evaluate prefix expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -26,6 +26,8 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		return evaluatePrefixExpression(node)
 	}
 
 	return nil
@@ -46,4 +48,39 @@ func nativeBoolToBooleanObject(input bool) *object.Boolean {
 		return TRUE
 	}
 	return FALSE
+}
+
+func evaluatePrefixExpression(expr *ast.PrefixExpression) object.Object {
+	right := Eval(expr.Right)
+
+	switch expr.Operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalMinusOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
 }
